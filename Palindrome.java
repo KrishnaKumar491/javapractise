02637a922ffc3254c2dc856fe8858aa565c4657c
KrishnaKumar <109646935+KrishnaKumar491@users.noreply.github.com>
2023-08-07 13:00:05 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "sahas";
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+    }
+}
+
+
+
+
+
+
